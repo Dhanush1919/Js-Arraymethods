141294966390e012693a3022568b3985bb557345
Dhanush1919 <dhanush.venkataraman@gmail.com>
2020-03-05 14:58:07 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
 const arr=[{name:'Dhanush',EmpID:90037},{name:'Kumar',EmpID:90038}];
+const on=arr.forEach(function(val)
+{
+  return val.name;
+}
+)

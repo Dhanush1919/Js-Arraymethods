6141ab06b37943013d82c530d99069561240c56a
Dhanush1919 <dhanush.venkataraman@gmail.com>
2020-03-05 14:50:55 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const arr=[{name:'Dhanush',EmpID:90037},{name:'Kumar',EmpID:90038}];
